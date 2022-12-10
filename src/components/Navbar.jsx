6e8dadbf9369f4b3ea5dd8d50f5c4f810961219a
the-name-romel <racopra@sorsu.edu.ph>
2022-12-10 02:13:01 -0500

    finalize hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="max-w-[1200px] h-[80px] items-center mx-auto flex justify-between">
-      <h1 className="text-[30px]">FLORIANE LAZARDE</h1>
-      <ul className="flex space-x-[40px] text-base">
-        <li>Home</li>
-        <li>Project</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+    <div className="fixed w-full">
+      <div className="max-w-[1200px] h-[80px] items-center mx-auto flex justify-between">
+        <h1 className="text-[30px]">FLORIANE LAZARDE</h1>
+        <ul className="flex space-x-[40px] text-[18px]">
+          <li>Home</li>
+          <li>Project</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { Profile } from "../img";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function () {
-  return <div className="h-screen w-full"></div>;
+  return (
+    <div className="h-screen max-w-[1200px] mx-auto flex items-center justify-between font-semibold">
+      <div className="flex flex-col">
+        <span className="text-[30px]">Hi, I'm</span>
+        <span className="text-[70px]">Floriane Lazarte</span>
+        <span className="text-[35px] opacity-50">
+          <Typewriter
+            words={["Aspiring web developer", "Beautiful", "Explorer"]}
+            loop={false}
+            cursor
+            cursorStyle="_"
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </span>
+        <div className="w-[155px] h-[60px] bg-[#1243F1] py-4 px-7 rounded-[10px] mt-[30px] hover:bg-opacity-70">
+          Contact me
+        </div>
+      </div>
+      <img src={Profile} alt="profile picture" />
+    </div>
+  );
 }
