032915fec59a4a67c60a8ab3f2fda0d5bbce6cf2
the-name-romel <racopra@sorsu.edu.ph>
2022-12-10 01:39:18 -0500

    add image and semi finalize navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div>
-      FLORAINE LAZARDE
-      <ul>
+    <div className="max-w-[1200px] h-[80px] items-center mx-auto flex justify-between">
+      <h1 className="text-[30px]">FLORIANE LAZARDE</h1>
+      <ul className="flex space-x-[40px] text-base">
         <li>Home</li>
         <li>Project</li>
         <li>About</li>
